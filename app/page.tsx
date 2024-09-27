969da091e6d9606cf3d4b43fcbc13b6c4ee35780
Daniar-git <daniar.78@inbox.ru>
2024-09-27 14:34:04 +0500

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,20 @@
+import { getHumeAccessToken } from "@/utils/getHumeAccessToken";
+import dynamic from "next/dynamic";
+
+const Chat = dynamic(() => import("@/components/Chat"), {
+  ssr: false,
+});
+
+export default async function Page() {
+  const accessToken = await getHumeAccessToken();
+
+  if (!accessToken) {
+    throw new Error();
+  }
+
+  return (
+    <div className={"grow flex flex-col"}>
+      <Chat accessToken={accessToken} configId="3816cda6-ebd1-4652-ac60-91e30354bbe8"/>
+    </div>
+  );
+}
